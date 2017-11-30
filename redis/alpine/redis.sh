a7e24d7b3b522210015cfa40393259020caaccc0
Ian Maddox <ian@maddox.mx>
2017-11-29 17:59:17 -0800

    Beginning cleanup

--- redis/alpine/redis.sh
+++ redis/alpine/redis.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+echo "Launching redis orchestrator"
 # Define config file locations
 SENTINEL_CONF=/etc/redis/sentinel.conf
 MASTER_CONF=/etc/redis/master.conf
