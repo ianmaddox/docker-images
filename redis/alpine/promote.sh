694155d7b9fec58c39f83ea3234a4dabbf8fd9f1
Ian Maddox <ian@maddox.mx>
2017-12-01 15:56:37 -0800

    Switching to discrete redis role tags

--- redis/alpine/promote.sh
+++ redis/alpine/promote.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 NEWMASTER=$1
-kubectl label --overwrite pod $NEWMASTER role=master
+kubectl label --overwrite pod $NEWMASTER redis-master="true"
+
+#todo: Monitor the replica pool. Once there are servers other than $NEWMASTER in there, remove the redis-slave flag
+#kubectl label --overwrite pod $NEWMASTER redis-master="true"
