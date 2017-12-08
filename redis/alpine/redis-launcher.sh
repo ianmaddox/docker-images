372057835eee9aad8725fead7b26fed8902df9d1
Ian Maddox <ian@maddox.mx>
2017-12-08 15:20:07 -0800

    Using a k8s-centric solution to discovering the first master

--- redis/alpine/redis-launcher.sh
+++ redis/alpine/redis-launcher.sh
@@ -41,7 +41,8 @@ function launchsentinel() {
   echo "Using config file $SENTINEL_CONF"
   while true; do
     # The sentinels must wait for a load-balanced master to appear then ask it for its actual IP.
-    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+#    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master=$(kubectl get pods -l redis-role=master -o=custom-columns=:..labels.podIP|xargs)
 
     if [[ -n ${master} ]]; then
       master="${master//\"}"
