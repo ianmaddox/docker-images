5eec2d302accb256dd74153732931f562e8db94a
Ian Maddox <ian@maddox.mx>
2017-12-08 16:28:15 -0800

    Cleaning up for publish

--- redis/alpine/label-updater.sh
+++ redis/alpine/label-updater.sh
@@ -7,6 +7,8 @@ if [ "$SENTINEL" ]; then
     exit
 fi
 
+kubectl label --overwrite pod $HOSTNAME redis-role="pending"
+
 RUNID=""
 while true; do
   RUNID=`redis-cli info server |grep run_id|awk -F: '{print $2}'|head -c6`
@@ -16,17 +18,4 @@ while true; do
   else
     sleep 1
   fi
-done
-
-# Start a daemon loop to keep the redis-role label updated
-ROLE=""
-LASTROLE="pending"
-while true; do
-  ROLE=`redis-cli info replication |grep role|awk -F: '{print $2}'|xargs`
-  if [ "$LASTROLE" != "$ROLE" ]; then
-    echo "Role changed from '$LASTROLE' to '$ROLE'"
-    kubectl label --overwrite pod $HOSTNAME redis-role="$ROLE"
-    LASTROLE=$ROLE
-  fi
-  sleep 1
 done
